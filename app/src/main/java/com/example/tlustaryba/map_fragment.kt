4ca50e0ee4359a8c5b42cca6e5307d9f5120dab7
Kacper Młynarski <kacpermln@wp.pl>
2022-06-18 20:15:34 +0200

    Update map_fragment.kt

--- app/src/main/java/com/example/tlustaryba/map_fragment.kt
+++ app/src/main/java/com/example/tlustaryba/map_fragment.kt
@@ -5,28 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.tlustaryba.databinding.FragmentMapFragmentBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class map_fragment : Fragment(), OnMapReadyCallback {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [map_fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class map_fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var googleMap: GoogleMap
+
+    lateinit var binding: FragmentMapFragmentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.onResume()
+        binding.mapView.getMapAsync(this)
     }
 
     override fun onCreateView(
@@ -37,23 +30,9 @@ class map_fragment : Fragment() {
         return inflater.inflate(R.layout.fragment_map_fragment, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment map_fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            map_fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onMapReady(map: GoogleMap) {
+        map?.let {
+            googleMap = it
+        }
     }
 }
